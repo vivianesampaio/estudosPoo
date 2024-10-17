83064e425fd6826e5a0044af65d67bd7a2786569
Viviane Sampaio <vivianesamsa@gmail.com>
2024-10-16 23:28:34 -0300

    create new classes

--- Conta-bancaria.ts
+++ Conta-bancaria.ts
@@ -9,14 +9,31 @@ class ContaBancaria {
 
     depositar(deposito: number) {
         this.saldo += deposito
-        console.log('Deposito ${deposito.tofixed(2)} realizado com sucesso!')
+        console.log('Deposito' , deposito.toFixed(2), 'realizado com sucesso!')
     }
 
     sacar(saque: number) {
         this.saldo -= saque
+        console.log('Saque', saque.toFixed(2), 'realizado com sucesso! Seu saldo atual é', this.saldo.toFixed(2))
     }
 
     mostrarSaldo() {
         console.log(this.saldo)
     }
 }
+
+let contaViviane = new ContaBancaria('Viviane', 123, 456, 1000)
+
+contaViviane.mostrarSaldo()
+
+contaViviane.depositar(500)
+
+contaViviane.mostrarSaldo()
+
+contaViviane.sacar(100.85)
+
+contaViviane.mostrarSaldo()
+
+contaViviane.depositar(100.85)
+
+contaViviane.mostrarSaldo()
